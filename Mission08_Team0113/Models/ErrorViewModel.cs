83e7e6fb503cea4a0b5c95030568bac7770056a7
jackelly7 <johnmcmillankelly@gmail.com>
2025-02-26 10:08:41 -0700

    Initial commit

---
+++ Mission08_Team0113/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team0113.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
