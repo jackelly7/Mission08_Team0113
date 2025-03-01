a967adaa3d9b740bf62842051c86a20ac40fc911
jackelly7 <johnmcmillankelly@gmail.com>
2025-02-28 20:28:48 -0700

    this

--- Mission08_Team0113/obj/Debug/net9.0/Mission08_Team0113.AssemblyInfo.cs
+++ Mission08_Team0113/obj/Debug/net9.0/Mission08_Team0113.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0113")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fb70feaf7859a71b9662455f44843d3728f3bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eaea5552cd9fc688ca9522fa4ec090d04ef6bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0113")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0113")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
